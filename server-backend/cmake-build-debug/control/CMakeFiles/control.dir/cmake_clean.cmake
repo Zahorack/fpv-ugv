5f91cd19b84276b2a79895af79d2c0c850ff7737
Oliver Holly <oliver.holly@dqi.sk>
2020-03-12 11:37:21 +0100

    remove ignored directories

--- server-backend/cmake-build-debug/control/CMakeFiles/control.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/control.dir/src/Control.cpp.obj"
-  "CMakeFiles/control.dir/src/Timer.cpp.obj"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/control.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
